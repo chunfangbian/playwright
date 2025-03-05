fef04bfe3af7cebdd8f805c1912c419846a1a254
Carol Bian <chunfangbian@gmail.com>
2025-03-05 22:20:43 +1100

    init

---
+++ lessons/lesson1.js
@@ -0,0 +1,17 @@
+// hello word
+console.log("hello world!")
+
+//variables
+var firstName = "John"
+let lastname = "Smith"
+console.log(lastname)
+
+
+var age,dateofbirth,sex
+age = "5"
+sex = "femal"
+console.log(sex)
+age = "6"
+console.log (age)
+
+//constants
